c729c448db2ab2674211ad805f9b2946b5e14302
josisusan <josisusan@gmail.com>
2014-02-05 20:50:42 +0545

    Method to add and display the content to the page.

--- app.rb
+++ app.rb
@@ -3,16 +3,13 @@ require "dm-core"
 require "dm-migrations"
 require "dm-sqlite-adapter"
 require "dm-timestamps"
+# require "pry"
 
 # config = YAML.load_file(File.join(Dir.pwd,'config.yml'))
 
 configure do
   DataMapper::setup(:default, File.join('sqlite3://', Dir.pwd, 'development.db'))
-end
-
-configure :development do
-  DataMapper.finalize
-  DataMapper.auto_upgrade!
+  # DataMapper::setup(:default, 'sqlite3:///home/susan/ruby/typing_tutor/development.db')
 end
 
 class ContentBlock
@@ -23,10 +20,34 @@ class ContentBlock
   property :created_at, DateTime
   property :updated_at, DateTime
 
+  def self.random_content
+    all.sample
+  end
 end
 
+# ContentBlock.auto_migrate!
+
+configure :development do
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+end
 
 get '/' do
   @title = 'Content Blocks'
+  @content = ContentBlock.random_content
   haml :index
+end
+
+get '/add_content' do
+  haml :add_content_form
+end
+
+post '/add_content' do
+  content_block = ContentBlock.new(params)
+  if content_block.save
+    @message = "sahi"
+  else
+    @message = "Not good"
+  end
+  haml :add_content_create
 end
